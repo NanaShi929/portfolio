06b66d811f0ed65e7e2ad73ff2b73cc812ac01b8
ShreYash <shreyashdevali929@gmail.com>
2024-10-10 20:14:22 +0530

    detail card

--- src/Components/Skeleton.js
+++ src/Components/Skeleton.js
@@ -2,11 +2,13 @@ import './Skeleton.css'
 function Skeleton() {
     return (
       <>
-        <div className='container'>
-          <div className="menuCard"></div>
+        <div className="page">
+          <div className="container">
+            <div className="menuCard"></div>
 
-          <div className="profileCard"></div>
-          <div className="detailsCard"></div>
+            <div className="profileCard"></div>
+            <div className="detailsCard"></div>
+          </div>
         </div>
       </>
     );
